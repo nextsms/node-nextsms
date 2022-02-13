955ca87f3b21b916733a3ff3cf1b6fb8668381d9
reubenwedson <reubenwedson@gmail.com>
2022-02-13 20:02:43 +0300

    test: add test for apiKey

--- tests/nextsms.test.ts
+++ tests/nextsms.test.ts
@@ -3,7 +3,13 @@ import NextSMS from '../src';
 describe('nextsms', () => {
   it('can instantiate nextsms class ', () => {
     // expect(sum(1, 1)).toEqual(2);
-    let nextsms = new NextSMS({ username: 'username', password: 'password', environment: 'testing' });
+    let nextsms = new NextSMS({ username: 'username', password: 'password', apiKey: null, environment: 'testing' });
+    expect(nextsms).toBeInstanceOf(NextSMS);
+  });
+
+  it('can instantiate nextsms class with apiKey ', () => {
+    // expect(sum(1, 1)).toEqual(2);
+    let nextsms = new NextSMS({ username: null, password: null, apiKey: 'apiKey', environment: 'testing' });
     expect(nextsms).toBeInstanceOf(NextSMS);
   });
 });
